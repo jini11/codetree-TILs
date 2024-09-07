ede654731a41637c5d122b12687d5eb1375b94d4
jini_11 <78905126+jini11@users.noreply.github.com>
2024-09-07 17:54:59 +0900

    Title: 데이터센터의 온도 조정, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

--- 240907/데이터센터의 온도 조정/adjusting-the-temperature-of-the-data-center.java
+++ 240907/데이터센터의 온도 조정/adjusting-the-temperature-of-the-data-center.java
@@ -1,60 +1,91 @@
+/*
+장비의 개수 N, 장비 작업량 C, G, H
+장비들이 선호하는 온도 범위
+
+선호 온도를 어떻게 구할 것인가
+
+*/
 import java.util.*;
+import java.io.*;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        
-        int n = sc.nextInt();
-        int c = sc.nextInt();
-        int g = sc.nextInt();
-        int h = sc.nextInt();
-        
-        List<int[]> dev = new ArrayList<>();
-        int minta = Integer.MAX_VALUE;
-        int maxtb = Integer.MIN_VALUE;
 
-        // 입력받은 구간을 리스트에 저장하고 minta, maxtb 계산
-        for (int i = 0; i < n; i++) {
-            int a = sc.nextInt();
-            int b = sc.nextInt();
-            
-            if (minta == Integer.MAX_VALUE) {
-                minta = a;
+    static int N, C, G, H, min, max;
+    static List<int[]> temp;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        C = Integer.parseInt(st.nextToken());
+        G = Integer.parseInt(st.nextToken());
+        H = Integer.parseInt(st.nextToken());
+
+        // temp = new ArrayList<>();
+        min = Integer.MAX_VALUE;
+        max = Integer.MIN_VALUE;
+
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            // min = Math.min(min, a);
+            // max = Math.max(max, b);
+            if (a == 0) {
+                if (map.containsKey(a)) {
+                    map.put(a, map.get(a) + G);
+                } else {
+                    map.put(a, G);
+                }
             } else {
-                minta = Math.min(minta, a);
+                if (map.containsKey(a)) {
+                    map.put(a, map.get(a) + G - C);
+                } else {
+                    map.put(s, G - C);
+                }
             }
-            maxtb = Math.max(maxtb, b);
-            
-            dev.add(new int[] {a, b});
-        }
-
-        Map<Integer, Integer> count = new HashMap<>();
-
-        // 각 구간에 대해 값 증가 처리
-        for (int[] pair : dev) {
-            int a = pair[0];
-            int b = pair[1];
-            
-            // g를 a~b 구간에 더하기
-            for (int num = a; num <= b; num++) {
-                count.put(num, count.getOrDefault(num, 0) + g);
+            if (s != 0) {
+                if (map.containsKey(0)) {
+                    map.put(0, map.get(0) + C);
+                } else {
+                    map.put(0, C);
+                }
             }
 
-            // c를 minta부터 a-1 구간에 더하기
-            for (int num = minta; num < a; num++) {
-                count.put(num, count.getOrDefault(num, 0) + c);
-            }
-
-            // h를 b+1부터 maxtb 구간에 더하기
-            for (int num = b + 1; num <= maxtb; num++) {
-                count.put(num, count.getOrDefault(num, 0) + h);
+            if (map.containsKey(b + 1)) {
+                map.put(b + 1, map.get(b + 1) + H - G);
+            } else {
+                map.put(b + 1, H - G);
             }
         }
-
-        // Map을 값 기준으로 정렬하고 가장 큰 값 찾기
-        int maxValue = Collections.max(count.values());
-
-        // 결과 출력
-        System.out.println(maxValue);
+        
+        // Map<Integer, Integer> map = new HashMap<>();
+        
+        // for (int[] tem : temp) {
+        //     for (int i = min; i < tem[0]; i++) {
+        //         map.put(i, map.getOrDefault(i, 0) + C);
+        //     }
+        //     for (int i = tem[0]; i <= tem[1]; i++) {
+        //         map.put(i, map.getOrDefault(i, 0) + G);
+        //     }
+        //     for (int i = tem[1] + 1; i <= max; i++) {
+        //         map.put(i, map.getOrDefault(i, 0) + H);
+        //     }
+        // }
+        // int res = 0;
+        // for (int num : map.values()) {
+        //     res = Math.max(res, num);
+        // }
+        // System.out.println(res);
+        temp = new ArrayList<>(map.keySet());
+        Collections.sort(temp);
+        int sum = 0;
+        int res = 0;
+        for (int key : temp) {
+            sum += map.get(key);
+            res = Math.max(res, sum);
+        }
+        System.out.println(res);
     }
 }
