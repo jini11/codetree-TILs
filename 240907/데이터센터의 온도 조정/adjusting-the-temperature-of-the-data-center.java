d07f5d33a8e32784d0bc510bf62baab75400bff7
jini_11 <78905126+jini11@users.noreply.github.com>
2024-09-07 17:48:48 +0900

    Title: 데이터센터의 온도 조정, Time: 1709ms, Memory: 176MB, Status: Memory Limit Exceed - CodeTree

--- 240907/데이터센터의 온도 조정/adjusting-the-temperature-of-the-data-center.java
+++ 240907/데이터센터의 온도 조정/adjusting-the-temperature-of-the-data-center.java
@@ -1,55 +1,60 @@
-/*
-장비의 개수 N, 장비 작업량 C, G, H
-장비들이 선호하는 온도 범위
-
-선호 온도를 어떻게 구할 것인가
-
-*/
 import java.util.*;
-import java.io.*;
 
 public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        int n = sc.nextInt();
+        int c = sc.nextInt();
+        int g = sc.nextInt();
+        int h = sc.nextInt();
+        
+        List<int[]> dev = new ArrayList<>();
+        int minta = Integer.MAX_VALUE;
+        int maxtb = Integer.MIN_VALUE;
 
-    static int N, C, G, H, max;
-    static int[][] temp;
-    static int[] all;
-
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        N = Integer.parseInt(st.nextToken());
-        C = Integer.parseInt(st.nextToken());
-        G = Integer.parseInt(st.nextToken());
-        H = Integer.parseInt(st.nextToken());
-
-        temp = new int[N][2];
-        all = new int[N * 2];
-        int idx = 0;
-        for (int i = 0; i < N; i++) {
-            st = new StringTokenizer(br.readLine());
-            temp[i][0] = Integer.parseInt(st.nextToken());
-            temp[i][1] = Integer.parseInt(st.nextToken());
-            all[idx++] = temp[i][0];
-            all[idx++] = temp[i][1];
+        // 입력받은 구간을 리스트에 저장하고 minta, maxtb 계산
+        for (int i = 0; i < n; i++) {
+            int a = sc.nextInt();
+            int b = sc.nextInt();
+            
+            if (minta == Integer.MAX_VALUE) {
+                minta = a;
+            } else {
+                minta = Math.min(minta, a);
+            }
+            maxtb = Math.max(maxtb, b);
+            
+            dev.add(new int[] {a, b});
         }
 
-        for (int i = 0; i < all.length; i++) {
-            max = Math.max(max, calc(all[i]));
-        }
-        System.out.println(max);
-    }
+        Map<Integer, Integer> count = new HashMap<>();
 
-    private static int calc(int range) {
-        int sum = 0;
-        for (int[] tem: temp) {
-            if (range < tem[0]) {
-                sum += C;
-            } else if (tem[0] <= range && range <= tem[1]) {
-                sum += G;
-            } else if (range > tem[1]) {
-                sum += H;
+        // 각 구간에 대해 값 증가 처리
+        for (int[] pair : dev) {
+            int a = pair[0];
+            int b = pair[1];
+            
+            // g를 a~b 구간에 더하기
+            for (int num = a; num <= b; num++) {
+                count.put(num, count.getOrDefault(num, 0) + g);
+            }
+
+            // c를 minta부터 a-1 구간에 더하기
+            for (int num = minta; num < a; num++) {
+                count.put(num, count.getOrDefault(num, 0) + c);
+            }
+
+            // h를 b+1부터 maxtb 구간에 더하기
+            for (int num = b + 1; num <= maxtb; num++) {
+                count.put(num, count.getOrDefault(num, 0) + h);
             }
         }
-        return sum;
+
+        // Map을 값 기준으로 정렬하고 가장 큰 값 찾기
+        int maxValue = Collections.max(count.values());
+
+        // 결과 출력
+        System.out.println(maxValue);
     }
 }
